ad7ae5cc2d4281cb7cad8ab9b46d1efff1282f82
Marc <kramleknod@gmail.com>
2026-08-22 16:29:05 +0200

    fix: update Supabase schema name

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,6 +4,6 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
   {
-    db: { schema: "leknod" },
+    db: { schema: "gatosdg" },
   },
 );
